e5831a2fa47eb0516c6abb0a49a72f74b91a4830
PabloDavidAR <pablo.arandarod@gmail.com>
2021-10-21 14:13:03 +0200

    Interface for reception and other utilities

--- src/communication.cpp
+++ src/communication.cpp
@@ -1,21 +1,35 @@
 #include <opencv2/opencv.hpp>
 
+#include "communication.hpp"
+
 namespace comm
 {
-    cv::Mat cameraInterface::cameraCallback(ConstImageStampedPtr &msg)
-    {
+    cameraInterface::cameraInterface() : imageReceived {false} 
+        {};
 
+    void cameraInterface::cameraCallback(ConstImageStampedPtr &msg)
+    {
         std::size_t width = msg->image().width();
         std::size_t height = msg->image().height();
         const char *data = msg->image().data().c_str();
         cv::Mat im(int(height), int(width), CV_8UC3, const_cast<char *>(data));
 
+        std::cout << "[CAMERA] Received image \n";
+        std::cout << "[CAMERA] Dimensions of the image are:  " << int(height) << ", " << int(width) << std::endl;
+
         im = im.clone();
         cv::cvtColor(im, im, cv::COLOR_RGB2BGR);
+
+        this->image = im;
     }
 
     cv::Mat cameraInterface::checkImage()
     {
         return this->image;
+    }
+
+    bool cameraInterface::receptionAccomplished()
+    {
+        return this->imageReceived;
     }
 }
--- src/communication.hpp
+++ src/communication.hpp
@@ -6,11 +6,14 @@ namespace comm
     class cameraInterface
     {
         public:
+            cameraInterface();
             void cameraCallback(ConstImageStampedPtr &msg);
             cv::Mat checkImage();
+            bool receptionAccomplished();
 
         private:
             cv::Mat image;
-    }
+            bool imageReceived;
+    };
 
 }
