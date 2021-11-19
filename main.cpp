1a8f12e68536db47fee29e4d7609c9146fc30b00
PabloDavidAR <pablo.arandarod@gmail.com>
2021-11-19 09:35:08 +0100

    Work with circles

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>  // for strtol
 
 #include "src/communication.cpp"
+#include "src/vectorMod.cpp"
 
 static boost::mutex mutex;
 const int key_left = 81;
@@ -19,20 +20,23 @@ const int maxParam1 {300};
 const int minParam1 {1};
 const double incrementParam {1};
 int param1 {100};
-int param2 {30};
+int param2 {35};
 const int maxRadius {200};
 const int minRadius {20};
+const int maxLengthCircleVector {20};
 
 int main(int argc, char ** argv)
 {
     // Declaration of global variables
     double stdDev {0.02};
     const double mean {0};
-    const int kernel_size {5};
+    const int kernel_size {7};
     bool show_image {true};
+    std::vector<std::vector<cv::Vec3f>> lastCircles;
+    std::vector<circleAvg> meanCenter;
     cv::Mat image, image_gray, filtered;
     comm::cameraInterface * camera (new comm::cameraInterface);
-    
+    comm::lidarInterface * lidar (new comm::lidarInterface);
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Argc check
@@ -47,11 +51,12 @@ int main(int argc, char ** argv)
     // Load Gazebo's client
     gazebo::client::setup(argc, argv);
     // Creation of node for communication
-    gazebo::transport::NodePtr node(new gazebo::transport::Node());
+    gazebo::transport::NodePtr node (new gazebo::transport::Node());
     node->Init();
 
-    // Listen to Gazebo topics
-    gazebo::transport::SubscriberPtr cameraSubscriber = node->Subscribe("~/pioneer2dx/camera/link/camera/image", &comm::cameraInterface::cameraCallback, camera);
+    // Create subscribers to listen to Gazebo topics
+    gazebo::transport::SubscriberPtr cameraSubscriber = node->Subscribe("~/pioneer2dx/camera/link/camera/image", &comm::cameraInterface::callbackMsg, camera);
+    gazebo::transport::SubscriberPtr lidarSubscriber = node->Subscribe("~/pioneer2dx/hokuyo/link/laser/scan", &comm::lidarInterface::callbackMsg, lidar);
 
     // Capability to publish to reset the world
     gazebo::transport::PublisherPtr worldPublisher = node->Advertise<gazebo::msgs::WorldControl>("~/world_control");
@@ -62,6 +67,7 @@ int main(int argc, char ** argv)
 
     // TODO Create publisher to publish the location of the closest marbel
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Pre-compute calibration matrixes
     cv::Matx33f K{1047, 0, 0.5*320,
                   0, 1047, 0.5*240,
@@ -69,8 +75,6 @@ int main(int argc, char ** argv)
     cv::Vec<float, 5> k(-0.25, 0.12, -0.00028, 0.00005, 0); // distortion coefficients
     cv::Size frameSize(320, 240);
     cv::Mat mapX, mapY;
-    std::cout << "The matrix K: \n" << K << std::endl;
-    std::cout << "The vector k:\n" << k << std::endl;
     cv::initUndistortRectifyMap(K, k, cv::Matx33f::eye(), K, frameSize, CV_32FC1,
                               mapX, mapY);
 
@@ -79,7 +83,7 @@ int main(int argc, char ** argv)
     while(true)
     {
         // Declare the local variables
-        cv::Mat gauss, median, blurred, sharp, laplacianGauss, laplacianBlur, laplacianMedian, laplacian, used, original, undistorted;
+        cv::Mat gauss, median, blurred, sharp, laplacianGauss, laplacianBlur, laplacianMedian, laplacian, used, original, undistorted, canny;
         std::vector<cv::Vec3f> circles, circlesLap;
 
         // Make it capable to read the image 
@@ -108,10 +112,10 @@ int main(int argc, char ** argv)
         //TODO Implement Rotation Mask
 
         // Don't do anything if you haven't received an image
-        if (!(camera->receptionAccomplished())) {continue;}
+        if (!(camera->receptionAccomplished()) | !(lidar->receptionAccomplished())) {continue;}
 
         // Obtain the image and correct distortion
-        original = camera->checkImage();
+        original = camera->checkReceived();
         //cv::undistort(original_, original,K,k);
         cv::remap(original, undistorted, mapX, mapY, cv::INTER_LANCZOS4);
         std::cout << "After the undistortion:  " << undistorted.channels() << std::endl;
@@ -128,18 +132,19 @@ int main(int argc, char ** argv)
         cv::Laplacian(median, laplacian, CV_32F, 5);
         median = median - 0.1*laplacian;
         median.convertTo(median, CV_8U);
+        cv::Canny(median, canny, 400, 450, 5, true);
 
         // Segmentation/recognition of marbles
-        cv::HoughCircles(median, circles, cv::HOUGH_GRADIENT, 1,
-                 median.rows/16,
+        cv::HoughCircles(canny , circles, cv::HOUGH_GRADIENT, 1,
+                 canny.rows/16,
                  param1, param2, minRadius, maxRadius 
         );
-        /* cv::HoughCircles(laplacian, circlesLap, cv::HOUGH_GRADIENT, 1,
-                 laplacian.rows/16,
-                 param1, param2, minRadius, maxRadius 
-        ); */
 
         std::cout << "Number of circles detected is: " << circles.size() << std::endl;
+
+        // Filtering of the circles
+        //pushBeginning<std::vector<cv::Vec3f>>(lastCircles, circles, maxLengthCircleVector);
+        pushBeginning<circleAvg>(meanCenter, averageCircle(circles), maxLengthCircleVector);
 
         // Using the LIDAR to find the marbles in the range
         
@@ -151,6 +156,8 @@ int main(int argc, char ** argv)
 
         if (show_image){
             // Draw circles detected in each image
+            cv::Mat avgCircleImage = median.clone();
+
             for (int i = 0; i < circles.size(); i++)
             {
                 // circle center
@@ -158,18 +165,25 @@ int main(int argc, char ** argv)
                 // circle outline
                 cv::circle( median, cv::Point(circles[i][0], circles[i][1]), circles[i][2], cv::Scalar(255,0,255), 1, cv::LINE_AA);
             }
+            cv::circle(avgCircleImage, cv::Point(meanCenter[maxLengthCircleVector-1].x_center, meanCenter[maxLengthCircleVector-1].y_center), meanCenter[maxLengthCircleVector-1].radius, cv::Scalar(255,0,255), 1, cv::LINE_AA);
 
-            mutex.lock();
+            /* mutex.lock();
             cv::imshow("Original", original);
             mutex.unlock();
             mutex.lock();
             cv::imshow("Undistorted", undistorted);
-            mutex.unlock();
+            mutex.unlock(); */
             mutex.lock();
             cv::imshow("Median", median);
             mutex.unlock();
-            mutex.lock();
+            /* mutex.lock();
             cv::imshow("Laplacian", laplacian);
+            mutex.unlock(); */
+            mutex.lock();
+            cv::imshow("Average Circle", avgCircleImage);
+            mutex.unlock();
+            mutex.lock();
+            cv::imshow("Canny edge", canny);
             mutex.unlock();
         }
     }
